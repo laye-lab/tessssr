acc2cecc4029cc4ab1f0aef72f19971e81b812d7
laye-lab <sndfa98@gmail.com>
2020-08-09 16:43:13 +0000

    test git

--- app/Http/Controllers/CalculheureController.php
+++ app/Http/Controllers/CalculheureController.php
@@ -127,6 +127,10 @@ class CalculheureController extends Controller
       ->select('nom')
       ->distinct('nom')
       ->get();
+      $testgit=DB::table('etablissement')
+      ->select('nom')
+      ->distinct('nom')
+      ->get();
 
       $array=DB::table('heures_supp')
       ->join('agent','agent.Matricule_Agent' ,'=','Agent_Matricule_Agent')
